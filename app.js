40a6ad266c1f74dffe949bd7a94caf83bf557641
saji-ryu <rsajiki@gmail.com>
2018-04-08 09:33:34 +0000

    調整

--- app.js
+++ app.js
@@ -12,7 +12,10 @@ const board = new five.Board({
 board.on("ready", function() {
     const servo = new five.Servo({
         pin: 'GPIO18',
-        startAt: 0
+        startAt:0,
+        invert: true,
+        range: [0,360],
+        pwmRange:[500,2400]
     });
     linda.io.on('connect', function(){
         console.log('connect!!!');
@@ -24,8 +27,8 @@ board.on("ready", function() {
         }, function(err, tuple){
             console.log("> " + tuple.data.message + " (from:" + tuple.from + ")");
             if(last_at + 5000 < Date.now()){
-                servo.to(120,800);
-                board.wait(4000,function () {
+                servo.to(270,800);
+                board.wait(2000,function () {
                     servo.to(0,800);
                     ts.write({
                         where: 'delta',
