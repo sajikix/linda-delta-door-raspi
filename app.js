ac9aae5f19df25fae0db470db1a2ebbc00ed9624
saji-ryu <rsajiki@gmail.com>
2018-04-15 20:51:58 +0900

    callback

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ linda.io.on('connect', () => {
     }, (err, tuple) => {
         console.log("> " + JSON.stringify(tuple.data) + " (from:" + tuple.from + ")");
         responseTuple = tuple.data;
-        if (err) {
+        if (err || last_at + 5000 > Date.now()) {
             responseTuple.response = 'error'
             ts.write(responseTuple);
         } else if (last_at + 5000 < Date.now()) {
@@ -34,8 +34,8 @@ linda.io.on('connect', () => {
             responseTuple.response = 'success_test';    //最後戻す
             console.log('> response=' + JSON.stringify(responseTuple));
             isOn = true;
-            moveServo();
-            ts.write(responseTuple);
+            moveServo(ts.write(responseTuple));
+
         }
     });
 
@@ -52,64 +52,13 @@ board.on("ready", () => {
         pwmRange: [500, 2400]
     });
 
-    moveServo = () => {
+    moveServo = (callback) => {
         console.log('moved');
         isOn = false;
         servo.to(270, 800);
         board.wait(2000, () => {
             servo.to(0, 800);
+            callback();
         });
     }
-
-    // if (isOn){
-    //     moveServo();
-    // }else{
-    //     console.log('no cmd');
-    // }
 });
-
-
-// board.on("ready", () => {
-//     const servo = new five.Servo({
-//         pin: 'GPIO18',
-//         startAt: 0,
-//         invert: true,
-//         range: [0, 360],
-//         pwmRange: [500, 2400]
-//     });
-//
-//     linda.io.on('connect', () => {
-//         console.log('connect!!!');
-//         let last_at = Date.now();
-//
-//         let moveServo = (callback) => {
-//             servo.to(270, 800);
-//             board.wait(2000, () => {
-//                 servo.to(0, 800);
-//                 callback();
-//             });
-//         }
-//
-//         ts.watch({
-//             where: "delta",
-//             type: "door",
-//             cmd: "open"
-//         }, (err, tuple) => {
-//             console.log("> " + JSON.stringify(tuple.data) + " (from:" + tuple.from + ")");
-//             let responseTuple = tuple.data;
-//             if (err) {
-//                 responseTuple.response = 'error'
-//                 ts.write(responseTuple);
-//             } else if (last_at + 5000 < Date.now()) {
-//                 last_at = Date.now();
-//                 responseTuple.response = 'success';
-//                 console.log('> response=' + JSON.stringify(responseTuple));
-//                 moveServo(() => {
-//                     ts.write(responseTuple);
-//                 });
-//             }
-//         });
-//
-//     });
-// });
-
